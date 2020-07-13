297d00c9fa76a5765b2b8ef61e09a830310bc67e
Asad Mahmood <asadmahmood1995@gmail.com>
2020-07-13 13:59:45 -0400

    Added skeleton.

---
+++ Scripts/objects/background.ts
@@ -0,0 +1,7 @@
+module objects {
+  export class Background extends createjs.Bitmap {
+    constructor(assetManager: createjs.LoadQueue) {
+      super(assetManager.getResult("background"));
+    }
+  }
+}

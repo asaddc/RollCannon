adddfc3e13ff79f38694cca3ebaa173a78ac2f4e
Brandon Ragnauth <officialbrandonragnauth@gmail.com>
2020-07-18 15:35:32 -0400

    Collision with level objects added

--- Scripts/objects/player.ts
+++ Scripts/objects/player.ts
@@ -40,9 +40,88 @@ module objects {
         this.y = 600 - this.halfH;
       }
 
-      if (this.y <= this.halfH) {
+      if (this.y <= this.halfH + 50) {
         // I have collided with the top boundary
-        this.y = this.halfH;
+        this.y = this.halfH + 50;
+      }
+
+      // SHOPPING CART
+      if (this.x > this.halfW + 239 && this.x < this.halfW + 300 && this.y < this.halfH + 100) {
+        // I have collided with the left side of the shopping cart
+        this.x = this.halfW + 239;
+      }
+
+      if (this.x > this.halfW + 300 && this.x < this.halfW + 340 && this.y < this.halfH + 100) {
+        // I have collided with the right  side of the shopping cart
+        this.x = this.halfW + 340;
+      }
+
+      if (this.x > this.halfW + 240 && this.x < this.halfW + 339 && this.y < this.halfH + 110) {
+        // I have collided with the bottom of the shopping cart
+        this.y = this.halfH + 110;
+      }
+
+      // APPLE COUNTER
+      if (this.x >= this.halfW + 70 && this.x <= this.halfW + 160 && this.y < this.halfH + 120) {
+        // I have collided with the top of the apple counter 
+        this.y = this.halfH + 50;
+      }
+
+      if (this.x >= this.halfW + 70 && this.x <= this.halfW + 160 && this.y > this.halfH + 180 && this.y < this.halfH + 200) {
+        // I have collided with the bottom of the apple counter 
+        this.y = this.halfH + 200;
+      }
+
+      if (this.x >= this.halfW + 70 && this.x <= this.halfW + 140 && this.y > this.halfH + 120 && this.y < this.halfH + 200) {
+        // I have collided with the left side of the apple counter 
+        this.x = this.halfW + 60;
+      }
+      if (this.x >= this.halfW + 140 && this.x < this.halfW + 180 && this.y > this.halfH + 100 && this.y < this.halfH + 200) {
+        // I have collided with the right side of the apple counter 
+        this.x = this.halfW + 165;
+      }
+
+      // TOP RIGHT PRODUCE
+      if (this.x >= 420 - this.halfW && this.y < 350 -this.halfH) {
+        // I have collided with the top-right produce boxes
+        this.x = 420 - this.halfW;
+      }
+
+      // PRODUCE PROTRUSION
+      if (this.x >= 275 - this.halfW && this.y > this.halfH + 200 && this.y < this.halfH + 240) {
+        // I have collided with the protruding produce from the top
+        this.y = this.halfH + 210;
+      }
+
+      if (this.x >= 275 - this.halfW && this.y > this.halfH + 270 && this.y < this.halfH + 330) {
+        // I have collided with the protruding produce from the bottom
+        this.y = this.halfH + 330;
+      }
+
+      if (this.x >= 275 - this.halfW  && this.y > this.halfH + 220 && this.y < this.halfH + 320) {
+        // I have collided with the protruding produce from the left
+        this.x = 275 - this.halfW;
+      }
+
+      // BOTTOM PRODUCTS
+      if (this.x >= this.halfW + 200 && this.y >= 540 - this.halfH) {
+        this.y = 540 - this.halfH;
+      }
+
+      // LEFT SHELVES
+      if (this.x < this.halfW + 125 && this.y > this.halfH + 190 && this.y <= this.halfH + 229) {
+        // I have collided with the left shelves from the top
+        this.y = this.halfH + 190;
+      }
+
+      if (this.x < this.halfW + 130 && this.y > this.halfH + 230 && this.y < this.halfH + 330) {
+        // I have collided with the left shelves from the top-right
+        this.x = this.halfW + 130;
+      }
+
+      if (this.x < this.halfW + 35 && this.y >= this.halfH + 336 && this.y <= this.halfH + 470) {
+        // I have collided with the left shelves from the right
+        this.x = this.halfW + 35;
       }
     }
   }
